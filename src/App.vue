ed8562a0a2880e57ecec20281c555639930dcc91
Victor Morales <victor.morales@fusiona.cl>
2022-11-14 13:02:25 -0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+import {ref} from 'vue'
+const textHolaMundo = "Hola Mundo";
+
+let count = ref(0);
+
+const sumar = () => {
+
+  count.value++;
+
+}
+
+
+const restar = () => {
+
+  count.value--;
+
+}
+
+
+const reset = () => {
+
+  count.value = 0;
+
+}
+
+</script>
+
+<template>
+  <h1>{{ textHolaMundo }} {{ count }}</h1>
+  <button @click="sumar">Sumar</button>
+  <button @click="restar">restar</button>
+  <button @click="reset">Rese</button>
+</template>
+
+<style scoped>
+</style>
